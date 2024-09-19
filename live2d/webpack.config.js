165c41f02a32956bbb15b04a3f487394f8332302
Buriburi <Buriburizaemon.may@gmail.com>
2024-09-20 00:26:08 +0200

    changed bundle

--- live2d/webpack.config.js
+++ live2d/webpack.config.js
@@ -6,5 +6,6 @@ module.exports = {
     filename: 'bundle.js',  // 打包输出的文件名
     path: path.resolve(__dirname, 'dist'),  // 输出的文件目录
   },
-  mode: 'development',  // 或 'production'，根据你的需求
+  // mode: 'development',  // 或 'production'，根据你的需求
+  mode : 'production'
 };
